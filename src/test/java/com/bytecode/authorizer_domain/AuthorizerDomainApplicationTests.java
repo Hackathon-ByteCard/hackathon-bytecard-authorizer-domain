4273924df005e0f51f0e66b1ffc0748dedf4d6a6
Diogo Geraldo <diogojunqueirageraldo@gmail.com>
2025-03-16 19:38:56 -0300

    first commit

---
+++ src/test/java/com/bytecode/authorizer_domain/AuthorizerDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytecode.authorizer_domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizerDomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
